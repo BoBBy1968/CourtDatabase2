24de6fa2111ba5c34c24288e737e2ca45764ce2c
BoBBy1968 <b.a.stanchev@gmail.com>
2020-11-19 19:47:26 +0200

    Validation in Expenses Models

--- CourtDatabase2/ViewModels/ExpenseInputViewModel.cs
+++ CourtDatabase2/ViewModels/ExpenseInputViewModel.cs
@@ -7,20 +7,23 @@ namespace CourtDatabase2.ViewModels
 {
     public class ExpenseInputViewModel
     {
-        [Required]
-        [MaxLength(50)]
+        [Required(ErrorMessage = "Получателят е задължителен.")]
+        [MaxLength(50, ErrorMessage = "Получателят може да бъде до 50 знака.")]
+        [MinLength(2, ErrorMessage ="Получателят трябва да бъде поне 2 знака.")]
         public string Payee { get; set; }
 
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "Описанието е задължително.")]
+        [MaxLength(100, ErrorMessage = "Описанието може да бъде до 100 знака.")]
+        [MinLength(3, ErrorMessage = "Описанието трябва да бъде поне 3 знака.")]
         public string ExpenceDescription { get; set; }
 
-        [Range(0.01, 100_000)]
+        [Range(0.01, 100_000, ErrorMessage = "Стойността трябва да бъде положително число до 100 000.")]
         [Column(TypeName = "decimal(18,2)")]
+        [Required(ErrorMessage = "Стойността е задължителна.")]
         public decimal ExpenceValue { get; set; }
 
         [DataType(DataType.Date)]
-        public DateTime ExpenceDate { get; set; }
+        public DateTime ExpenceDate { get; set; } = DateTime.Now.Date;
 
         public int LawCaseId { get; set; }
 
